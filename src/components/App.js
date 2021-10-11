d1fbb934b606d194d9af7b57a52b908955f8eea6
satyajit2o <41511102+satyajit2o@users.noreply.github.com>
2021-10-11 16:18:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,18 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  let[msg,setmsg]=useState("");
+ function  setmessage(){
+    if(msg===""){
+      setmsg(msg=<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
+    }
+    
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      
+      <button id="click" onClick={setmessage}>click</button>
+       {msg}
     </div>
   );
 }
